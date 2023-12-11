8a73510b8eaf5e8e8e2a9747d2edd124fcdb974d
Erudite885 <erudite885@gmail.com>
2023-12-11 19:37:01 +1300

    App function

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,8 @@
 import React from 'react'
 
-import RatingComponent from "./components/RatingComponent"
-
 const App: React.FC = () => {
   return (
-    <div className='bg-black bg-opacity-95 w-[100vw] h-full'>
-        
-      <div className='h-full flex justify-center items-center'>
-        <RatingComponent />
-      </div>
-    </div>
+    <div>App</div>
   )
 }
 
